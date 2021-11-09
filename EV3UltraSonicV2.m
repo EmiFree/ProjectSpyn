571b4202ce7765518698558929aa64b12264bef0
TonyJustinM <94008635+TonyJustinM@users.noreply.github.com>
2021-11-09 12:16:48 -0700

    Update EV3UltraSonicV2.m

--- EV3UltraSonicV2.m
+++ EV3UltraSonicV2.m
@@ -8,43 +8,44 @@ while state ~= 6
     distanceWall = 22;
     
     %deviation from center desired
-    distanceRoom = 1.5;
+    distanceRoom = 0.5;
     
     %Distance to indicate when to turn right.
     distanceMax = 50;
     
     %Equations to slow down the speed as it gets closer to the center
-    driftLeftEquation = 30 + (wallDistance / 15);
-    driftRightEquation = 33 - (wallDistance / 15);
+    driftLeftEquation = 30 + (wallDistance / 20);
+    driftRightEquation = (19 / wallDistance) + 30;
     
     disp(wallDistance);
     
     disp(wallDistance);
     if wallDistance > (distanceWall - distanceRoom) && wallDistance < (distanceWall + distanceRoom) %Keepmoving forward
-       
-        fprintf("1");
-        brick.MoveMotorAngleRel('A', 30, 200, 'Brake');
-        brick.MoveMotorAngleRel('B', 30, 200, 'Brake');
-        pause(1);
+       %if statement used to keep moving forward if it is in the center and
+       %room inside the room of error.
+            fprintf("Moving Forward");
+            brick.MoveMotorAngleRel('A', -30, 200, 'Brake');
+            brick.MoveMotorAngleRel('B', -30, 200, 'Brake');
+            pause(1);
         
         
     elseif wallDistance > (distanceWall + distanceRoom) %Drifiting left
-        fprintf("2");
+        fprintf("Drifting Left");
        % while wallDistance > (distanceWall - distanceRoom) 
-            brick.MoveMotorAngleRel('A', (driftLeftEquation), 100, 'Brake');
-            brick.MoveMotorAngleRel('B', 30, 100, 'Brake');
+            brick.MoveMotorAngleRel('B', -(driftLeftEquation), 100, 'Brake');
+            brick.MoveMotorAngleRel('A', -30, 100, 'Brake');
             pause(1);
        % end
         
 
    elseif (wallDistance > distanceWall) % turning right
-      %  fprintf("turning right");
+         % fprintf("turning right");
         
-   elseif wallDistance < (distanceWall + distanceRoom) %Drifitng right
-          fprintf("3");
+   elseif wallDistance < (distanceWall - distanceRoom) %Drifitng right
+          fprintf("Drifting Right");
         
-          brick.MoveMotorAngleRel('A', 30, 150, 'Brake');
-          brick.MoveMotorAngleRel('B', (driftRightEquation), 150, 'Brake');
+          brick.MoveMotorAngleRel('B', -30, 150, 'Brake');
+          brick.MoveMotorAngleRel('A', -(driftRightEquation), 150, 'Brake');
           pause(1);
 
         
