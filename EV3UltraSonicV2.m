da32b9a8d2d97fdbf425c8d187962da2892366ce
TonyJustinM <94008635+TonyJustinM@users.noreply.github.com>
2021-11-16 11:48:39 -0700

    Update EV3UltraSonicV2.m

--- EV3UltraSonicV2.m
+++ EV3UltraSonicV2.m
@@ -8,47 +8,45 @@ while state ~= 6
     distanceWall = 22;
     
     %deviation from center desired
-    distanceRoom = 1.5;
+    distanceRoom = 6;
     
     %Distance to indicate when to turn right.
     distanceMax = 50;
     
+    speedA = -25;
+    speedB = -25;
     %Equations to slow down the speed as it gets closer to the center
-    driftLeftEquation = 31 + (wallDistance / 20);
-    driftRightEquation = (20 / wallDistance) + 31;
+    driftLeftEquation = -speedA + (wallDistance / 20);
+    driftRightEquation = (20 / wallDistance) - speedB;
     
     disp(wallDistance);
     
     disp(wallDistance);
     if wallDistance > (distanceWall - distanceRoom) && wallDistance < (distanceWall + distanceRoom) %Keepmoving forward
-       %if statement used to keep moving forward if it is in the center and
-       %room inside the room of error.
+       %if statement used to keep moving forward if it is in the center and room inside the room of error.
             fprintf("Moving Forward");
-            brick.MoveMotor('A', -30);
-            brick.MoveMotor('B', -30);
+            brick.MoveMotor('A', speedA);
+            brick.MoveMotor('B', speedB);
             pause(1);
             
         
         
     elseif wallDistance > (distanceWall + distanceRoom) %Drifiting left
         fprintf("Drifting Left");
-       % while wallDistance > (distanceWall - distanceRoom) 
-            brick.MoveMotor('B', -(driftLeftEquation));
-            brick.MoveMotor('A', -30);
-            pause(1);
-            
-       % end
-        
+      
+           brick.MoveMotor('B', -(driftLeftEquation));
+           brick.MoveMotor('A', speedA);
+           pause(1);
 
    elseif (wallDistance > distanceWall) % turning right
          % fprintf("turning right");
         
    elseif wallDistance < (distanceWall - distanceRoom) %Drifitng right
           fprintf("Drifting Right");
-        
-          brick.MoveMotor('B', -30);
-          brick.MoveMotor('A', -(driftRightEquation));
-          pause(1);
+      
+             brick.MoveMotor('B', speedB);
+             brick.MoveMotor('A', -(driftRightEquation));
+             pause(1);
           
 
         
