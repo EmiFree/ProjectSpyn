55e9fae905bc48e120f6ccddb8a68f3d6a6b7d56
EmiFree <52217493+EmiFree@users.noreply.github.com>
2021-11-18 10:15:44 -0800

    Create UltraCheckV3.m

---
+++ UltraCheckV3.m
@@ -0,0 +1,49 @@
+function ultraCheck(brick)
+    wallDistance =  brick.UltrasonicDist(3);
+    global speedA;
+    global speedB;
+    %where we would want the robot to be around
+    distanceWall = 22;
+    
+    %deviation from center desired
+    distanceRoom = 1;
+    
+    %Distance to indicate when to turn right.
+    distanceMax = 40;
+    
+    %Equations to slow down the speed as it gets closer to the center
+    driftLeftEquation = -speedA + (27 / wallDistance^3);
+    driftRightEquation = ( 2 / 2^wallDistance) - speedB;
+    
+    disp(wallDistance);
+    
+    disp(wallDistance);
+    if wallDistance > (distanceWall - distanceRoom) && wallDistance < (distanceWall + distanceRoom) %Keepmoving forward
+       %if statement used to keep moving forward if it is in the center and room inside the room of error.
+            fprintf("Moving Forward");
+            
+            
+        
+        
+    elseif wallDistance > (distanceWall + distanceRoom) && wallDistance < distanceMax %Drifiting left
+        fprintf("Drifting Left");
+      
+           speedA = -(driftLeftEquation);
+          
+
+   elseif (wallDistance > distanceMax) % turning right
+         fprintf("turning right");
+         turn(brick, 90);
+        
+   elseif wallDistance < (distanceWall - distanceRoom) %Drifitng right
+          fprintf("Drifting Right");
+      
+             
+             speedB = -(driftRightEquation);
+             
+          
+
+        
+   end
+
+end
